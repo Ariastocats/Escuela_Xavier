6c0c2cefcd5fd4fd5e575f2fc4e65321c407c690
unknown <luisarias888angel@gmail.com>
2023-06-05 03:20:57 -0600

    Actualizar Servlete mutante

--- src/main/java/Controlador/ServletMutante.java
+++ src/main/java/Controlador/ServletMutante.java
@@ -2,10 +2,11 @@ package Controlador;
 
 import Datos.MutanteDAO;
 import Modelo.MutanteJB;
-import java.io.IOException;
-import java.util.List;
-import javax.servlet.ServletException;
+import java.io.*;
 import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.util.*;
+import javax.servlet.*;
 import javax.servlet.http.*;
 
 @WebServlet("/ServletMutante")
@@ -14,11 +15,21 @@ public class ServletMutante extends HttpServlet{
 	private static final long serialVersionUID = 1L;
 	
 	@Override
-    protected void doGet(HttpServletRequest hsreq, HttpServletResponse hsres)
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
     		throws ServletException, IOException {
-		 List<MutanteJB> Mutantes = new MutanteDAO().seleccionar();
-		 System.out.println("Mutantes = " + Mutantes);
-		 hsreq.setAttribute("Mutantes", Mutantes);
-		 hsreq.getRequestDispatcher("index.jsp").forward(hsreq, hsres);
+		 MutanteDAO mut= new MutanteDAO();
+		 List<MutanteJB> lista=mut.seljoin();
+		 if(lista.isEmpty()) {
+			 System.out.println("vacia la lista");
+		 }
+		 else {
+			 System.out.println("tienes datos");
+			 
+		 }
+		 System.out.println("mosttando");
+		 request.setAttribute("lista",lista);
+		 RequestDispatcher dispatcher = request.getRequestDispatcher("MutanteList.jsp");
+		 dispatcher.forward(request,response);
+		 
 	}
 }
