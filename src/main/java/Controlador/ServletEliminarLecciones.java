7695d3613ea774ceb4e7d5d9923b9ac0be28c9a6
unknown <luisarias888angel@gmail.com>
2023-06-08 04:10:05 -0600

    servlet para eliminar lecciones

---
+++ src/main/java/Controlador/ServletEliminarLecciones.java
@@ -0,0 +1,28 @@
+package Controlador;
+
+import Datos.LeccionesDAO;
+import Modelo.LeccionesJB;
+import java.io.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+@WebServlet("/ServletEliminarLecciones")
+public class ServletEliminarLecciones extends HttpServlet{
+private static final long serialVersionUID = 1L;
+	
+	@Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+		int id_leccion = Integer.parseInt(request.getParameter("id_leccion"));
+		LeccionesDAO lece= new LeccionesDAO();
+		lece.borrar(id_leccion);
+        response.sendRedirect("ServletLeccionesList");
+	}
+
+}
