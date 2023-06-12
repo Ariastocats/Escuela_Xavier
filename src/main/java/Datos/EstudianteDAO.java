79c1fa1c7e9fa76c15ab051b3da0f74672bb56e7
unknown <luisarias888angel@gmail.com>
2023-06-12 02:31:26 -0600

    Actualizar estudiante DAO

--- src/main/java/Datos/EstudianteDAO.java
+++ src/main/java/Datos/EstudianteDAO.java
@@ -4,13 +4,16 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
-import Modelo.ProfesorJB;
+
 public class EstudianteDAO {
 	public static final String selectSQL = "SELECT * FROM estudiantes";
-	public static final String insertSQL = "Insert into Estudiante(Curp,Nombre,Apellido_Pat,Apellido_Mat,Poder,N_Alias,fecha_nac,cel,direcc,Rol,Correo_Inst,Contraseña,Status) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
-	public static final String updateSQL = "UPDATE Estudiante SET Curp=?,Nombre=?,Apellido_Pat=?,Apellido_Mat=?,Poder=?,N_Alias=?,fecha_nac=?,cel=?,direcc=?,Rol=?,Correo_Inst=?,Contraseña=?,Status=? WHERE Matricula=?";
-	public static final String deleteSQL = "DELETE FROM Estudiante WHERE Matricula=?";
+	public static final String insertSQL = "Insert into Estudiantes(Curp,Nombre,Apellido_Pat,Apellido_Mat,Poder,N_Alias,fecha_nac,cel,direcc,Rol,Correo_Inst,Contraseña,Status) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	public static final String updateSQL = "UPDATE Estudiantes SET Nombre=?,Apellido_Pat=?,Apellido_Mat=?,Poder=?,N_Alias=?,fecha_nac=?,cel=?,direcc=?,Rol=?,Correo_Inst=?,Contraseña=?,Status=? WHERE Matricula=?";
+	public static final String update2SQL= "UPDATE Estudiantes SET Nombre=?,Apellido_Pat=?,Apellido_Mat=?,Poder=?,N_Alias=?,fecha_nac=?,cel=?,direcc=?,Rol=? WHERE Curp=?";
+	public static final String deleteSQL = "DELETE FROM Estudiantes WHERE Matricula=?";
 	public static final String joinest="SELECT estudiantes.curp,estudiantes.nombre,estudiantes.apellido_pat,estudiantes.apellido_mat,estudiantes.poder,estudiantes.n_alias,estudiantes.fecha_nac,estudiantes.cel,estudiantes.direcc, rol.rol as Rols,estudiantes.matricula, estudiantes.correo_inst,estudiantes.contraseña,estudiantes.status FROM estudiantes JOIN rol ON estudiantes.rol=rol.id_rol";
+	public static final String updatebuscar = "SELECT * FROM Estudiantes WHERE matricula=?";
+	public static final String eliminar="UPDATE Estudiantes set Status='deshabilitado' where curp=?";
 	
 	public List<EstudianteJB> seleccionar(){
         Connection conn = null;
@@ -189,13 +192,85 @@ public class EstudianteDAO {
 			
 			Conexion.close(state);
 			Conexion.close(conn);
-			ProfesorJB estudianteDelete = new ProfesorJB();
+			EstudianteJB estudianteDelete = new EstudianteJB();
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return registros;
 	}
+	
+	public int dardebaja(int curp) {
+		Connection conn = null;
+		PreparedStatement state = null;
+		int registros = 0;
+		
+		try {
+			conn = Conexion.getConnection();
+			state = conn.prepareStatement(eliminar);
+		
+			
+			state.setInt(1,curp);
+			
+			registros = state.executeUpdate();
+			if(registros>0)
+				System.out.println("Registro actualizado");
+			
+			Conexion.close(state);
+			Conexion.close(conn);
+			EstudianteJB estMod = new EstudianteJB();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return registros;
+	}
+	
+	public EstudianteJB buscar(int id){
+        Connection conn = null;
+        PreparedStatement state = null;
+        ResultSet result = null;
+        EstudianteJB mut = null;
+
+        try {
+            conn = Conexion.getConnection();
+            state = conn.prepareStatement(updatebuscar);
+
+            state.setInt(1,id);
+
+            result = state.executeQuery();
+
+            while(result.next()) {
+            	
+                int curp = result.getInt("curp");
+                String nombre = result.getString("nombre");
+                String apellido_pat = result.getString("apellido_pat");
+                String apellido_mat = result.getString("apellido_mat");
+                String poder = result.getString("poder");
+                String n_alias = result.getString("n_alias");
+                Date fecha_nac = result.getDate("fecha_nac");
+				String cel = result.getString("cel");
+				String direcc=result.getString("direcc");
+				int rol=result.getInt("rol");
+				int Matricula=result.getInt("Matricula");
+				String Correo_Inst = result.getString("Correo_Inst");
+				String Contraseña = result.getString("Contraseña");
+				String Status = result.getString("Status");
+                
+
+                System.out.println("encontramos los valores");
+                mut = new EstudianteJB(curp,nombre,apellido_pat,apellido_mat,poder,n_alias,fecha_nac,cel,direcc,rol,Matricula,Correo_Inst,Contraseña,Status);
+            }
+            Conexion.close(result);
+            Conexion.close(state);
+            Conexion.close(conn);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return mut;
+    }
 	
 	public int modificar(EstudianteJB est) {
 		Connection conn = null;
@@ -205,20 +280,52 @@ public class EstudianteDAO {
 		try {
 			conn = Conexion.getConnection();
 			state = conn.prepareStatement(updateSQL);
-			state.setInt(1,est.getCurp());
-			state.setString(2,est.getNombre());
-			state.setString(3,est.getApellido_Pat());
-			state.setString(4,est.getApellido_Mat());
-			state.setString(5,est.getPoder());
-			state.setString(6,est.getN_Alias());
-			state.setDate(7,est.getFecha_nac());
-			state.setString(8,est.getCel());
-			state.setString(9,est.getDirecc());
-			state.setInt(10,est.getRol());
-			state.setString(13,est.getCorreo_Inst());
-			state.setString(14,est.getContraseña());
-			state.setString(15,est.getStatus());
-			state.setInt(16,est.getMatricula());
+			state.setString(1,est.getNombre());
+			state.setString(2,est.getApellido_Pat());
+			state.setString(3,est.getApellido_Mat());
+			state.setString(4,est.getPoder());
+			state.setString(5,est.getN_Alias());
+			state.setDate(6,est.getFecha_nac());
+			state.setString(7,est.getCel());
+			state.setString(8,est.getDirecc());
+			state.setInt(9,est.getRol());
+			state.setString(10,est.getCorreo_Inst());
+			state.setString(11,est.getContraseña());
+			state.setString(12,est.getStatus());
+			state.setInt(13,est.getMatricula());
+			
+			registros = state.executeUpdate();
+			if(registros>0)
+				System.out.println("Registro actualizado");
+			
+			Conexion.close(state);
+			Conexion.close(conn);
+			EstudianteJB estudianteMod = new EstudianteJB();
+			
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return registros;
+	}
+	
+	public int modificar2(EstudianteJB est) {
+		Connection conn = null;
+		PreparedStatement state = null;
+		int registros = 0;
+		
+		try {
+			conn = Conexion.getConnection();
+			state = conn.prepareStatement(update2SQL);
+			state.setString(1,est.getNombre());
+			state.setString(2,est.getApellido_Pat());
+			state.setString(3,est.getApellido_Mat());
+			state.setString(4,est.getPoder());
+			state.setString(5,est.getN_Alias());
+			state.setDate(6,est.getFecha_nac());
+			state.setString(7,est.getCel());
+			state.setString(8,est.getDirecc());
+			state.setInt(9,est.getRol());
+			state.setInt(10,est.getCurp());
 			
 			registros = state.executeUpdate();
 			if(registros>0)
