0d6cf3cd7bd19bbbf2eed665c4350e47dd64f2ac
unknown <luisarias888angel@gmail.com>
2023-06-12 02:33:22 -0600

    Actualizar dao de mutante

--- src/main/java/Datos/MutanteDAO.java
+++ src/main/java/Datos/MutanteDAO.java
@@ -1,4 +1,5 @@
 package Datos;
+
 import Modelo.MutanteJB;
 import java.sql.*;
 import java.util.ArrayList;
@@ -9,6 +10,8 @@ public class MutanteDAO {
 	public static final String updateSQL = "UPDATE Mutante SET Nombre=?,Apellido_Pat=?,Apellido_Mat=?,Poder=?,N_Alias=?,fecha_nac=?,cel=?,direcc=?,Rol=? WHERE Curp=?";
 	public static final String deleteSQL = "DELETE FROM Mutante WHERE Curp=?";
 	public static final String seljoin= "SELECT mutante.curp,mutante.nombre,mutante.apellido_pat,mutante.apellido_mat,mutante.poder,mutante.n_alias,mutante.fecha_nac,mutante.cel,mutante.direcc, rol.rol as Rols FROM only mutante JOIN rol ON rol.id_rol=mutante.rol ORDER BY mutante.curp";
+	public static final String updatebuscar = "SELECT * FROM Mutante WHERE Curp=?";
+	
 	
 	public List<MutanteJB> seleccionar(){
         Connection conn = null;
@@ -124,10 +127,11 @@ public class MutanteDAO {
 		Connection conn = null;
 		PreparedStatement state = null;
 		int registros = 0;
+		int idObtenido=0;
 		
 		try {
 			conn = Conexion.getConnection();
-			state = conn.prepareStatement(insertSQL);
+			 state = conn.prepareStatement(insertSQL, Statement.RETURN_GENERATED_KEYS);
 			
 			state.setString(1,Mutantes.getNombre());
 			state.setString(2,Mutantes.getApellido_Pat());
@@ -141,6 +145,13 @@ public class MutanteDAO {
 			
 			registros = state.executeUpdate();
 			if(registros>0) {
+				 ResultSet generatedKeys = state.getGeneratedKeys();
+	                if (generatedKeys.next()) {
+	                    idObtenido = generatedKeys.getInt(1);
+	                    System.out.println("ID generada: " + idObtenido);
+	                }
+				
+				
 				System.out.println("Registro agregado correctamente");
 			}
 			
@@ -151,10 +162,10 @@ public class MutanteDAO {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		return registros;
+		return idObtenido;
 	}
 	
-	public int borrar(MutanteJB Mutantes) {
+	public int borrar(int curp) {
 		Connection conn = null;
 		PreparedStatement state = null;
 		int registros = 0;
@@ -163,7 +174,7 @@ public class MutanteDAO {
 			conn = Conexion.getConnection();
 			state = conn.prepareStatement(deleteSQL);
 			
-			state.setInt(1,Mutantes.getCurp());
+			state.setInt(1,curp);
 			registros = state.executeUpdate();
 			
 			if(registros>0) {
@@ -179,6 +190,47 @@ public class MutanteDAO {
 		}
 		return registros;
 	}
+	
+	public MutanteJB buscar(int id){
+        Connection conn = null;
+        PreparedStatement state = null;
+        ResultSet result = null;
+        MutanteJB mut = null;
+
+        try {
+            conn = Conexion.getConnection();
+            state = conn.prepareStatement(updatebuscar);
+
+            state.setInt(1,id);
+
+            result = state.executeQuery();
+
+            while(result.next()) {
+                int curp = result.getInt("curp");
+                String nombre = result.getString("nombre");
+                String apellido_pat = result.getString("apellido_pat");
+                String apellido_mat = result.getString("apellido_mat");
+                String poder = result.getString("poder");
+                String n_alias = result.getString("n_alias");
+                Date fecha_nac = result.getDate("fecha_nac");
+				String cel = result.getString("cel");
+				String direcc=result.getString("direcc");
+				int rol=result.getInt("rol");
+                
+
+                System.out.println("encontramos los valores");
+                mut = new MutanteJB(curp,nombre,apellido_pat,apellido_mat,poder,n_alias,fecha_nac,cel,direcc,rol);
+            }
+            Conexion.close(result);
+            Conexion.close(state);
+            Conexion.close(conn);
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return mut;
+    }
 	
 	public int modificar(MutanteJB Mutantes) {
 		Connection conn = null;
@@ -202,9 +254,12 @@ public class MutanteDAO {
 			
 			
 			registros = state.executeUpdate();
-			if(registros>0)
+			if(registros>0) {
 				System.out.println("Registro actualizado");
-			
+			}
+			else {
+				System.out.println("Registro no actualizado");
+			}
 			Conexion.close(state);
 			Conexion.close(conn);
 			MutanteJB mutanteMod = new MutanteJB();
